a2ba6ba7021a189e6127bfcde972cb529614356a
jejem8430 <jer.munoz30@gmail.com>
2023-01-21 11:01:37 +0100

    node 4

---
+++ server.js
@@ -0,0 +1,4 @@
+require('dotenv').config() // this line is mandatory when you want to read variables from the '.env' file 
+console.log(
+  `I am ${process.env.NAME}, wilder in ${process.env.CITY}, and I love ${process.env.LANGAGE} `
+);
